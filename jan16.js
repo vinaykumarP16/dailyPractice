df4a6fe9a5690237040e22c2c07b9f59ee556381
vinaykumarp16 <vinaykumar.p1995v@gmail.com>
2023-01-16 20:35:47 +0530

    first commit jan 16th day-1

---
+++ jan16.js
@@ -0,0 +1,32 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("hello");
+newArray.push("vinay");
+newArray.pop();
+console.log(newArray);
